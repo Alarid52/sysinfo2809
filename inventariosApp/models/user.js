734452d40601749266e66aee2c76a388fdc9e537
Alarid52 <alarid152@gmail.com>
2021-03-08 12:57:48 -0600

    Clase 08/03/2021

---
+++ inventariosApp/models/user.js
@@ -0,0 +1,20 @@
+const login = (email,passwd,callback)=>{
+    var err ='';
+    var bd_data=''; //simula la info de bd
+
+    if ( email == 'joe@doe.com' && passwd == '1234') {
+        //consulta bd info faltante
+        bd_data={
+            'email': email,
+            'depto': 'Ventas',
+            'phone': '5511447788'
+        }
+    } else {
+        err = {
+            'mensaje': "Credenciales incorrectas"
+        }
+    }
+    callback(err,bd_data);
+}
+
+exports.login = login;
