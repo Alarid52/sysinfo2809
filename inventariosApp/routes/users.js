d6cb823ce8334e7d4da4664c374f072b8ba884de
Alarid52 <alarid152@gmail.com>
2021-03-10 12:57:12 -0600

    Clase 10/03/2021

--- inventariosApp/routes/users.js
+++ inventariosApp/routes/users.js
@@ -7,8 +7,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//Proceso de autenticación pero sin elementos de SEGURIDAD
 router.post('/login', (req,res,next) => {
+  //console.log(req.body.email , req.body.passwd);
+  usuario.login(req.body.email,req.body.passwd,( e, d )=>{ //passwd SHA256 (algoritmo HASH de resumen)
+    if (d) {
+      res.send('Login correcto');
+      ses = req.session;
+      console.log(ses.id);
 
+
+    } else {
+      res.json(e);
+    }
+  });
 });
 
 module.exports = router;
